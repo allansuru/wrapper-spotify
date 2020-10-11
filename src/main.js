328a0dc6176a5d0ed9af256231798c42b8bc26e2
SuRu <allansuru@hotmail.com>
2020-10-11 16:08:35 -0300

    corrigindo apontamentos do lint

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-export const search = () => { }
+export const search = () => { };
 
-export const searchAlbuns = () => { }
+export const searchAlbuns = () => { };
 
-export const searchArtist = () => { }
+export const searchArtist = () => { };
 
-export const searchTracks = () => { }
+export const searchTracks = () => { };
 
-export const searchPlaylists = () => { }
+export const searchPlaylists = () => { };
