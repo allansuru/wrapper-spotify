822e96f06fec9b27ef9457982a57106599c2f889
SuRu <allansuru@hotmail.com>
2020-10-11 16:07:54 -0300

    adicionando smoke tests

---
+++ src/main.js
@@ -0,0 +1,9 @@
+export const search = () => { }
+
+export const searchAlbuns = () => { }
+
+export const searchArtist = () => { }
+
+export const searchTracks = () => { }
+
+export const searchPlaylists = () => { }
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,29 @@
+/* eslint-disable indent */
 import { expect } from 'chai';
+import { search, searchAlbuns, searchArtist, searchTracks, searchPlaylists } from '../src/main';
 
-describe('Main', () => {
+describe('Spotify Wrapper', () => {
+
+    describe('smoke test', () => {
+        it('should exist the search method', () => {
+            expect(search).to.exist;
+        });
+
+        it('should exist the searchAlbuns method', () => {
+            expect(searchAlbuns).to.exist;
+        });
+
+        it('should exist the searchArtist method', () => {
+            expect(searchArtist).to.exist;
+        });
+
+        it('should exist the searchTracks method', () => {
+            expect(searchTracks).to.exist;
+        });
+
+        it('should exist the searchPlaylists method', () => {
+            expect(searchPlaylists).to.exist;
+        });
+    });
 
 });
