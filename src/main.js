1d30807de801de754386a36af8ea395397a38ddf
SuRu <allansuru@hotmail.com>
2020-10-12 17:29:47 -0300

    instalando novas dependencias para trabalhr com unit test de serviços, sinon, sinonChai e sinosStubPromise

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-export const search = () => { };
+// eslint-disable-next-line no-undef
+export const search = (q, type) => fetch(`https://api.spotify.com/v1/search?q=${q}&type=${type}`);
 
 export const searchAlbuns = () => { };
 
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,9 +1,18 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-expressions */
 /* eslint-disable indent */
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import { search, searchAlbuns, searchArtist, searchTracks, searchPlaylists } from '../src/main';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require('node-fetch');
 
 describe('Spotify Wrapper', () => {
-
     describe('smoke test', () => {
         it('should exist the search method', () => {
             expect(search).to.exist;
@@ -25,5 +34,31 @@ describe('Spotify Wrapper', () => {
             expect(searchPlaylists).to.exist;
         });
     });
+    describe('Generic search', () => {
+        let fetchedStub;
+        before(() => {
+            fetchedStub = sinon.stub(global, 'fetch');
+        });
 
+        it('should call fetch function', () => {
+
+            const artists = search();
+
+            expect(fetchedStub).to.have.been.calledOnce;
+        });
+
+        it('it should receive the correct url to fetch', () => {
+            const artist = search('Incubus', 'artist');
+
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+
+            const album = search('Incubus', 'album');
+
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album');
+        });
+
+        after(() => {
+            fetchedStub.restore();
+        });
+    });
 });
