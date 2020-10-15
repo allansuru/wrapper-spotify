7c54a46673b7158702951b7198a8940454bd4963
SuRu <allansuru@hotmail.com>
2020-10-15 14:01:49 -0300

    ajuste lint

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
+/* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
-export const search = (q, type) =>
-    fetch(`https://api.spotify.com/v1/search?q=${q}&type=${type}`)
-        .then((data) => data.json())
-        .catch(error => error);
+export const search = (q, type) = fetch(`https://api.spotify.com/v1/search?q=${q}&type=${type}`)
+    .then(data => data.json())
+    .catch(error => error);
 
 export const searchAlbuns = () => { };
 
