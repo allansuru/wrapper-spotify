f306416d62d4ee3dce442e0c4313f5fe939fc10a
SuRu <allansuru@hotmail.com>
2020-10-17 17:01:23 -0300

    ajuste lint

--- exemples/album.js
+++ exemples/album.js
@@ -4,4 +4,6 @@ import { searchAlbuns } from '../src/main';
 
 const albums = searchAlbuns('incubus');
 
-albums.then(data => console.log(data.albums.items.map(({ name }) => console.log(name)))).catch(err => err);
+albums
+    .then(data => console.log(data.albums.items.map(({ name }) => console.log(name))))
+    .catch(err => err);
